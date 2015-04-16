30c29d80d330bbfc44ada47622a8a0a87aac4d41
Marcz55 <marwa828@student.liu.se>
2015-04-16 17:28:46 +0200

    Skickar kontinuerligt testdata

--- SPI-test/SPI-test/SPI-test.c
+++ SPI-test/SPI-test/SPI-test.c
@@ -22,24 +22,112 @@ void spiTransmit(unsigned char data)
 	 inbuffer = SPDR;
 }
 
+
+void transmitData(int header_, int dataByte1, int dataByte2_)
+{
+    PORTA = (0<<PORTA2); //Slave select
+    spiTransmit(header_);
+   	PORTA = (1<<PORTA2); //Slave deselect
+   	_delay_us(5);
+   	PORTA = (0<<PORTA2);
+   	spiTransmit(dataByte1);
+    PORTA = (1<<PORTA2);
+   	_delay_us(5);
+   	PORTA = (0<<PORTA2);
+   	spiTransmit(dataByte2_);
+   	PORTA = (1<<PORTA2);
+   	_delay_us(5);
+}
+
 int main(void)
 {
-	spiInit();
-	PORTA = (0<<PORTA2); //Slave select
-	int p = 0;
-	while(p<100)
-	{
-	_delay_us(5);
-	PORTA = (0<<PORTA2); //Slave select
-	spiTransmit(0x44);
-	PORTA = (1<<PORTA2); //Slave deselect
-	p = p+1;
-	}
-	while(1)
-	{
-		_delay_ms(1000);
-		PORTA = (0<<PORTA2); //Slave select
-		spiTransmit(48);
-		PORTA = (1<<PORTA2); //Slave deselect
-	}
+    int dataOffset = -3;
+    int dataIterator = 0;
+    int leakTestByte = 0;
+    int nodeTestByte1 = 0b00010011;
+    int nodeTestByte2 = 0b00001100;
+    while(1)
+    {
+	    spiInit(); 
+    	PORTA = (0<<PORTA2); 
+    	_delay_us(5);
+        
+        transmitData(202,0b00000001,0b01110000 + 2 * dataOffset +152); // Sida 1
+
+        transmitData(232,0b10000001,0b00000111 + 2 * dataOffset + 55);// Vinkel 1
+        
+        
+        
+        transmitData(208,0b00000001,0b01110000 + dataOffset); // Sida 2
+
+    	transmitData(240,0b10000001,0b00000111 + dataOffset); // Vinkel 2
+        
+        
+        
+        transmitData(216,0b00000001,0b01110000 + 2 * dataOffset +152); // Sida 3
+
+        transmitData(248,0b10000001,0b00000111 + 2 * dataOffset + 55);// Vinkel 3
+        
+        
+        
+        transmitData(224,0b00000001,0b01110000 + dataOffset); // Sida 4
+
+        transmitData(136,0b10000001,0b00000111 + dataOffset); // Vinkel 4
+        
+        
+        
+        transmitData(144,0b10000001,0b00000111 + dataOffset); // Vinkel total
+        
+        
+        transmitData(152,0,leakTestByte);
+ 
+ 
+    	transmitData(160,nodeTestByte1,nodeTestByte2); // Nod
+
+        switch(dataIterator)
+        {
+            case 0:
+            dataIterator ++;
+            dataOffset = 3;
+            break;
+            case 1:
+            dataIterator ++;
+            dataOffset = 1;
+            break;
+            case 2:
+            dataIterator ++;
+            dataOffset = -2;
+            break;
+            case 3:
+            dataIterator ++;
+            dataOffset = 0;
+            break;
+            case 4:
+            dataIterator = 0;
+            dataOffset = -3;
+            if (leakTestByte == 0)
+            {
+                leakTestByte = 1;
+            }
+            else
+            {
+                leakTestByte = 0;
+            }
+            if (nodeTestByte1 == 0b00010011)
+            {
+                nodeTestByte1 = 0b0001100;
+                nodeTestByte2 = 0b00101000;
+            }
+            else
+            {
+                nodeTestByte1 = 0b00010011;
+                nodeTestByte2 = 0b00001100;
+            }
+            break;
+            default:
+            dataIterator = 0;
+            break;
+        }
+        _delay_ms(200);
+    }    
 }
