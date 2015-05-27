bfa8bbb11675f86514e1bdf5de3492a6c1f9b051
Isak Wiberg <isawi527@student.liu.se>
2015-05-27 18:04:45 +0200

    Added some comments to files.

--- Test_servo_v1/Test_servo_v1/SPI.c
+++ Test_servo_v1/Test_servo_v1/SPI.c
@@ -1,10 +1,11 @@
 ﻿/*
  * SPI.c
  *
- * Created: 4/20/2015 11:26:24 AM
- *  Author: marwa828
+ * Skapad: 4/20/2015 11:26:24 AM
+ * Author: Marcus Wälivaara
+ * Version: 1.0
+ *
  */ 
-
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -14,19 +15,30 @@
 #include "SPI.h"
 #include "nodsystemet.h"
 
+/*
+ * Initieringsfunktion som körs på styrenheten. 
+ */
 void spiMasterInit(void)
 {
-    DDRA = (1<<PORTA2)|(1<<PORTA1); // Denna port skickar slave-select till en annan processor.
+    DDRA = (1<<PORTA2) | (1<<PORTA1); // Denna port skickar slave-select till en annan processor.
     PORTA = (1<<PORTA2); //Ha ingen slav vald
-    DDRB = (1<<PORTB5)|(1<<PORTB7)|(0<<PORTB4); //Definiera outputs. PortB4 skall hållas hög på master.
-    SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0); //Sätt enhet till master, enable spi, klockfrekvens
+    DDRB = (1<<PORTB5) | (1<<PORTB7) | (0<<PORTB4); //Definiera outputs. PortB4 skall hållas hög på master.
+    SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0); //Sätt enhet till master, enable spi, klockfrekvens
 }
-
+/*
+ * Initieringsfunktion som körs på kommunikations- och
+ * sensorenheten. 
+ */
 void spiSlaveInit()
 {
     DDRB = (1<<PORTB6); //Alla utom MISO ska vara ingångar.
-    SPCR = (1<<SPE)|(1<<SPIE); //Sätt på SPI    
+    SPCR = (1<<SPE) | (1<<SPIE); //Sätt på SPI    
 }    
+/*
+ * Hjälpfunktion för att skicka ett litet datapaket till 
+ * sensorsenheten. Används endast av 
+ * transmitDataToSensorUnit 
+ */
 void spiTransmitToSensorUnit(unsigned char data)
 {
     cli();
@@ -37,7 +49,11 @@ void spiTransmitToSensorUnit(unsigned char data)
     PORTA = (1<<PORTA2) | (1<<PORTA1); //Slave deselect
     sei();
 }
-
+/*
+ * Hjälpfunktion för att skicka ett litet datapaket till 
+ * kommunikationsenheten. Används endast av 
+ * transmitDataToCommUnit 
+ */
 void spiTransmitToCommUnit(unsigned char data)
 {
     cli();
@@ -48,9 +64,11 @@ void spiTransmitToCommUnit(unsigned char data)
     PORTA = (1<<PORTA1) | (1<<PORTA2); //Slave deselect
     sei();
 }
-
-
-void transmitDataToCommUnit(int header_, int data) // Lagt till "interrupt disabled"
+/*
+ * Skickar data till kommunikationsenheten. Den här funktionen är 
+ * den som används av styrenheten.
+ */
+void transmitDataToCommUnit(int header_, int data) 
 {
 	cli();
     uint8_t lowDataByte = data;
@@ -63,11 +81,10 @@ void transmitDataToCommUnit(int header_, int data) // Lagt till "interrupt disab
     _delay_us(5);
 	sei();
 }
-
-
-
-
-void transmitDataToSensorUnit(int header_, int data) // Lagt till "interrupt disabled"
+/*
+ * Skickar data till sensorenheten 
+ */
+void transmitDataToSensorUnit(int header_, int data) 
 {
 	cli();
     uint8_t lowDataByte = data;
@@ -81,8 +98,9 @@ void transmitDataToSensorUnit(int header_, int data) // Lagt till "interrupt dis
     sei();
 }
 
-
-// Ger tillbaka värde motsvarande den headern som skickades.
+/*
+ * Ger tillbaka värde motsvarande den headern som skickades.
+ */
 int fetchDataFromSensorUnit(int header_)
 {
     uint8_t lowDataByte;
